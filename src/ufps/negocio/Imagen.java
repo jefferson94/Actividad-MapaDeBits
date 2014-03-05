d8f768f19a3b095c81529b21661b14c26b3693e3
ValeriaG <ValeriaG@ValeriaG-PC>
2014-03-05 18:08:02 -0500

    MetodoFlip

--- src/ufps/negocio/Imagen.java
+++ src/ufps/negocio/Imagen.java
@@ -83,7 +83,7 @@ public class Imagen {
      */ 
     public void flipHorizontal(){
         int i=0;
-        int j=filaColores.get(i).getPixeles().length()-1;
+        int j=filaColores.get(i).pixeles.length()-1;
         
         for (; i<filaColores.length()/2;i++,j--){
              FilaColor aux= filaColores.get(i);
